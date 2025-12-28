4cd13bcd7207e95994e9aa7536d3fb93acd03269
nazimmestar <nazim.mestar.11@gmail.com>
2025-12-28 12:24:46 +0100

    editing path images

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -4,7 +4,7 @@ const Search = ({SearchTerm , setSearchTerm}) => {
   return (
     <div className="search">
       <div>
-        <img src="search.svg" alt="search" />
+        <img src="/search.svg" alt="search" />
         <input 
         type="text"
         placeholder='Search for a movie'
